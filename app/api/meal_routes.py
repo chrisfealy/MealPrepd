f89fab662396377afe1d8abf4689ec5b4a2c7aca
chrisfealy <chrismfealy@gmail.com>
2024-02-29 12:08:14 -0600

    meal routes + form

--- app/api/meal_routes.py
+++ app/api/meal_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import current_user, login_required
 from ..models import db, Meal
+from ..forms import MealForm
 
 meal_routes = Blueprint('meals', __name__)
 
@@ -25,4 +26,36 @@ def get_current_user_meals():
 @meal_routes.route('/new', methods=['POST'])
 @login_required
 def create_meal():
-    pass
+    form = MealForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        params = {
+            'name': form.data['name'],
+            'description': form.data['description'],
+            'image_url': form.data['image_url']
+        }
+        meal = Meal(**params)
+        db.session.add(meal)
+        db.session.commit()
+        return meal.to_dict()
+    return {'Error': 'Bad Request'}, 400
+
+@meal_routes.route('/<int:id>/edit', methods=['PUT'])
+@login_required
+def update_food(id):
+    meal = Meal.query.get(id)
+    form = MealForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if not meal:
+        return {'message':"Meal couldn't be found"}, 404
+
+    if current_user.id != meal.user_id:
+        return {'message':'Forbidden'}, 401
+
+    if form.validate_on_submit():
+        meal.name = form.data['name']
+        meal.description = form.data['description']
+        db.session.commit()
+        return meal.to_dict()
+    return {'Error': 'Bad Request'}, 400
