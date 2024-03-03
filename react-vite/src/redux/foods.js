494b54b281b67bcf32a96662b47c4129923cd658
chrisfealy <chrismfealy@gmail.com>
2024-03-03 15:45:31 -0600

    redux bug fixes

--- react-vite/src/redux/foods.js
+++ react-vite/src/redux/foods.js
@@ -4,122 +4,122 @@ export const UPDATE_FOOD = 'spots/UPDATE_FOOD'
 export const REMOVE_FOOD = 'spots/REMOVE_FOOD'
 
 export const loadFoods = (foods) => ({
-    type: LOAD_FOODS,
-    foods
+  type: LOAD_FOODS,
+  foods
 })
 
 export const receiveFood = (food) => ({
-    type: RECEIVE_FOOD,
-    food
+  type: RECEIVE_FOOD,
+  food
 })
 
 export const updateFood = (food) => ({
-    type: UPDATE_FOOD,
-    food
+  type: UPDATE_FOOD,
+  food
 })
 
 export const deleteFood = (foodId) => ({
-    type: REMOVE_FOOD,
-    foodId
+  type: REMOVE_FOOD,
+  foodId
 })
 
 export const thunkGetFoods = () => async (dispatch) => {
-    const response = await fetch('/api/foods')
+  const response = await fetch('/api/foods')
 
-    if(response.ok) {
-        const foods = await response.json()
-        dispatch(loadFoods(foods))
-        return foods
-    }
+  if (response.ok) {
+    const foods = await response.json()
+    dispatch(loadFoods(foods))
+    return foods
+  }
 }
 
 export const thunkGetUserFoods = () => async (dispatch) => {
   const response = await fetch('/api/foods/current')
 
-  if(response.ok) {
-      const foods = await response.json()
-      dispatch(loadFoods(foods))
-      return foods
+  if (response.ok) {
+    const foods = await response.json()
+    dispatch(loadFoods(foods))
+    return foods
   }
 }
 
 export const thunkGetFood = (foodId) => async (dispatch) => {
-    const response = await fetch(`/api/foods/${foodId}`)
+  const response = await fetch(`/api/foods/${foodId}`)
 
-    if(response.ok) {
-        const food = await response.json()
-        dispatch(receiveFood(food))
-        return food
-    }
+  if (response.ok) {
+    const food = await response.json()
+    dispatch(receiveFood(food))
+    return food
+  }
 }
 
 export const thunkCreateFood = (food) => async (dispatch) => {
-    const response = await fetch('/api/foods/new', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(food)
-    })
+  const response = await fetch('/api/foods/new', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(food)
+  })
 
-    if(response.ok) {
-      const food = await response.json()
-      dispatch(receiveFood(food))
-      return food
-    }
-    else {
-      const error = await response.json()
-      return error
-    }
+  if (response.ok) {
+    const food = await response.json()
+    dispatch(receiveFood(food))
+    return food
+  }
+  else {
+    const error = await response.json()
+    return error
+  }
 }
 
 export const thunkUpdateFood = (food) => async (dispatch) => {
-    const response = await fetch(`/api/foods/${food.id}/edit`, {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(food)
-    })
+  const response = await fetch(`/api/foods/${food.id}/edit`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(food)
+  })
 
-    if(response.ok) {
-      const food = await response.json()
-      dispatch(updateFood(food))
-      return food
-    }
-    else {
-      const error = await response.json()
-      return error
-    }
+  if (response.ok) {
+    const food = await response.json()
+    dispatch(updateFood(food))
+    return food
+  }
+  else {
+    const error = await response.json()
+    return error
+  }
 }
 
 export const thunkDeleteFood = (foodId) => async (dispatch) => {
-    const response = await fetch(`/api/foods/${foodId}`, {
-      method: 'DELETE'
-    })
+  const response = await fetch(`/api/foods/${foodId}`, {
+    method: 'DELETE'
+  })
 
-    if(response.ok) {
-      dispatch(deleteFood(foodId))
-    }
+  if (response.ok) {
+    dispatch(deleteFood(foodId))
+  }
 }
 
 const foodsReducer = (state = {}, action) => {
-    switch(action.type) {
-        case LOAD_FOODS: {
-            const newState = {}
-            action.foods.foods.forEach((food) => {
-              newState[food.id] = food
-            })
-            return newState
-        }
-        case RECEIVE_FOOD:
-            return { ...state, [action.food.id]: action.food }
-        case UPDATE_FOOD:
-            return { ...state, [action.food.id]: action.food }
-        case REMOVE_FOOD: {
-            const newState = { ...state }
-            delete newState[action.spotId]
-            return newState
-        }
-        default:
-            return state
+  switch (action.type) {
+    case LOAD_FOODS: {
+      const newState = {}
+      action.foods.foods.forEach((food) => {
+        newState[food.id] = food
+      })
+      return newState
     }
+    case RECEIVE_FOOD:
+      return { ...state, [action.food.id]: action.food }
+    case UPDATE_FOOD:
+      return { ...state, [action.food.id]: action.food }
+    case REMOVE_FOOD: {
+      const newState = { ...state }
+      delete newState[action.foodId]
+      return newState
+    }
+    default:
+      return state
+  }
 }
 
 export default foodsReducer
--- react-vite/src/redux/meals.js
+++ react-vite/src/redux/meals.js
@@ -4,121 +4,121 @@ export const UPDATE_MEAL = 'spots/UPDATE_MEAL'
 export const REMOVE_MEAL = 'spots/REMOVE_MEAL'
 
 export const loadMeals = (meals) => ({
-    type: LOAD_MEALS,
-    meals
+  type: LOAD_MEALS,
+  meals
 })
 
 export const receiveMeal = (meal) => ({
-    type: RECEIVE_MEAL,
-    meal
+  type: RECEIVE_MEAL,
+  meal
 })
 
 export const updateMeal = (meal) => ({
-    type: UPDATE_MEAL,
-    meal
+  type: UPDATE_MEAL,
+  meal
 })
 
 export const deleteMeal = (mealId) => ({
-    type: REMOVE_MEAL,
-    mealId
+  type: REMOVE_MEAL,
+  mealId
 })
 
 export const thunkGetMeals = () => async (dispatch) => {
-    const response = await fetch('/api/meals')
+  const response = await fetch('/api/meals')
 
-    if(response.ok) {
-        const meals = await response.json()
-        dispatch(loadMeals(meals))
-        return meals
-    }
+  if (response.ok) {
+    const meals = await response.json()
+    dispatch(loadMeals(meals))
+    return meals
+  }
 }
 
 export const thunkGetUserMeals = () => async (dispatch) => {
   const response = await fetch('/api/meals/current')
 
-  if(response.ok) {
-      const meals = await response.json()
-      dispatch(loadMeals(meals))
-      return meals
+  if (response.ok) {
+    const meals = await response.json()
+    dispatch(loadMeals(meals))
+    return meals
   }
 }
 
 export const thunkGetMeal = (mealId) => async (dispatch) => {
-    const response = await fetch(`/api/meals/${mealId}`)
+  const response = await fetch(`/api/meals/${mealId}`)
 
-    if(response.ok) {
-        const meal = await response.json()
-        dispatch(receiveMeal(meal))
-        return meal
-    }
+  if (response.ok) {
+    const meal = await response.json()
+    dispatch(receiveMeal(meal))
+    return meal
+  }
 }
 
 export const thunkCreateMeal = (meal) => async (dispatch) => {
-    const response = await fetch('/api/meals/new', {
-      method: 'POST',
-      body: meal
-    })
+  const response = await fetch('/api/meals/new', {
+    method: 'POST',
+    body: meal
+  })
 
-    if(response.ok) {
-      const meal = await response.json()
-      dispatch(receiveMeal(meal))
-      return meal
-    }
-    else {
-      const error = await response.json()
-      return error
-    }
+  if (response.ok) {
+    const meal = await response.json()
+    dispatch(receiveMeal(meal))
+    return meal
+  }
+  else {
+    const error = await response.json()
+    return error
+  }
 }
 
 export const thunkUpdateMeal = (meal) => async (dispatch) => {
-    const response = await fetch(`/api/meals/${meal.id}/edit`, {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(meal)
-    })
+  const response = await fetch(`/api/meals/${meal.id}/edit`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(meal)
+  })
 
-    if(response.ok) {
-      const meal = await response.json()
-      dispatch(updateMeal(meal))
-      return meal
-    }
-    else {
-      const error = await response.json()
-      return error
-    }
+  if (response.ok) {
+    const meal = await response.json()
+    dispatch(updateMeal(meal))
+    return meal
+  }
+  else {
+    const error = await response.json()
+    return error
+  }
 }
 
 export const thunkDeleteMeal = (mealId) => async (dispatch) => {
-    const response = await fetch(`/api/meals/${mealId}`, {
-      method: 'DELETE'
-    })
+  const response = await fetch(`/api/meals/${mealId}`, {
+    method: 'DELETE'
+  })
 
-    if(response.ok) {
-      dispatch(deleteMeal(mealId))
-    }
+  if (response.ok) {
+    dispatch(deleteMeal(mealId))
+  }
 }
 
 const mealsReducer = (state = {}, action) => {
-    switch(action.type) {
-        case LOAD_MEALS: {
-            const newState = {}
-            action.meals.meals.forEach((meal) => {
-              newState[meal.id] = meal
-            })
-            return newState
-        }
-        case RECEIVE_MEAL:
-            return { ...state, [action.meal.id]: action.meal }
-        case UPDATE_MEAL:
-            return { ...state, [action.meal.id]: action.meal }
-        case REMOVE_MEAL: {
-            const newState = { ...state }
-            delete newState[action.mealId]
-            return newState
-        }
-        default:
-            return state
+  switch (action.type) {
+    case LOAD_MEALS: {
+      const newState = {}
+      action.meals.meals.forEach((meal) => {
+        newState[meal.id] = meal
+      })
+      return newState
     }
+    case RECEIVE_MEAL:
+      return { ...state, [action.meal.id]: action.meal }
+    case UPDATE_MEAL:
+      return { ...state, [action.meal.id]: action.meal }
+    case REMOVE_MEAL: {
+      const newState = { ...state }
+      delete newState[action.mealId]
+      return newState
+    }
+    default:
+      return state
+  }
 }
 
 export default mealsReducer
