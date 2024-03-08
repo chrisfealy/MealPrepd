10f9481c4dd9a5987583d850cd1eb49b840f53bb
chrisfealy <chrismfealy@gmail.com>
2024-03-08 15:50:30 -0600

    prevent logged out user from accessing create food form

--- react-vite/src/components/CreateFood/CreateFood.jsx
+++ react-vite/src/components/CreateFood/CreateFood.jsx
@@ -1,6 +1,17 @@
+import { useSelector } from 'react-redux'
 import FoodForm from '../FoodForm'
+import { useNavigate } from 'react-router-dom'
 
 function CreateFood() {
+    const navigate = useNavigate()
+    const user = useSelector(state => state.session.user)
+
+    if(!user) {
+        return (
+            <h2>Access Denied</h2>
+        )
+    }
+
     const food = {
         name: '',
         serving_size: '',
