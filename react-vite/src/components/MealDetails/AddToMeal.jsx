b7f497d42fafb0d8319b92fbc54d199cf5eaf0f5
chrisfealy <chrismfealy@gmail.com>
2024-03-08 16:12:08 -0600

    add food to meals styling

--- react-vite/src/components/MealDetails/AddToMeal.jsx
+++ react-vite/src/components/MealDetails/AddToMeal.jsx
@@ -3,8 +3,9 @@ import { useDispatch, useSelector } from "react-redux"
 import { thunkGetFoods } from "../../redux/foods"
 import { addFoodToMeal, thunkGetMeal } from "../../redux/meals"
 import { useModal } from "../../context/Modal"
+import './MealDetails.css'
 
-function AddToMeal({mealId}) {
+function AddToMeal({ mealId }) {
     const dispatch = useDispatch()
     const { closeModal } = useModal()
     const foods = useSelector(state => Object.values(state.foods))
@@ -20,18 +21,18 @@ function AddToMeal({mealId}) {
             .then(() => {
                 dispatch(thunkGetMeal(mealId))
             })
-            .then(() => closeModal())
+            .then(closeModal)
     }
 
     return (
-        <ul>
+        <div className="add-foods-container">
             {foods.map(food => (
-                <div key={food.id}>
+                <div key={food.id} className="add-foods-food">
                     <p>{food.name}</p>
                     <button onClick={(e) => addToMeal(e, food)}>Add Food to Meal</button>
                 </div>
             ))}
-        </ul>
+        </div>
     )
 }
 
